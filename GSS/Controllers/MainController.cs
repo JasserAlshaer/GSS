79e92ae476908a583338e77f31e5bf1637f2d0d1
ASUS <ASUS@DESKTOP-SV986PK>
2024-04-21 21:04:07 +0300

    implementation

--- GSS/Controllers/MainController.cs
+++ GSS/Controllers/MainController.cs
@@ -1,4 +1,5 @@
-﻿using GSS.Models;
+﻿using GSS.DTO;
+using GSS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -42,19 +43,58 @@ namespace GSS.Controllers
         }
         public IActionResult Students()
         {
-            return View();
+
+            return View(_context.Users.Where(x => x.UserTypeId == 3).ToList());
         }
         public IActionResult Employees()
         {
-            return View();
+            var response = from emp in _context.Users
+                           where emp.UserTypeId == 2
+                           select new EmployeeCardDTO
+                           {
+                               Id = emp.Id,
+                               Name = emp.Name,
+                               Email = emp.Email,
+                               Phone = emp.Phone,
+                               DepartmentName = emp.Department.Name
+                           };
+            var res1 = _context.Users.Where(x => x.UserTypeId == 2).Include
+                (x => x.Department).ToList();
+            return View(response.ToList());
         }
         public IActionResult Reports()
         {
-            return View();
+            return View(_context.Reports.ToList());
         }
         public IActionResult Invoices()
         {
-            return View();
+            return View(_context.Invoices.ToList());
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            var report = _context.Reports.FirstOrDefault(x => x.Id == id);
+            return View(report);
+        }
+        [HttpPut]
+        public IActionResult Edit(int id,string title,DateTime requestdate,float DueAmount)
+        {
+            var report = _context.Reports.FirstOrDefault(x => x.Id == id);
+            report.Title = title;
+            report.DueAmount = DueAmount;
+            report.RequetDate = requestdate;
+            _context.Update(report);
+            _context.SaveChanges();
+            return RedirectToAction("Reports");
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var report = _context.Reports.FirstOrDefault(x => x.Id == id);
+            _context.Remove(report);
+            _context.SaveChanges();
+            return RedirectToAction("Reports");
         }
     }
 }
